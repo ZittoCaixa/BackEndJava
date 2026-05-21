42714e1d7a8e0a6e8bb9d337cf95285a0d94ebac
ZittoCaixa <C078969@corp.caixa.gov.br>
2026-05-21 09:34:31 -0300

    Finalização

--- src/test/java/com/backendjava/simulacao/resource/SimulacaoResourceTest.java
+++ src/test/java/com/backendjava/simulacao/resource/SimulacaoResourceTest.java
@@ -4,6 +4,9 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasItem;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
@@ -51,7 +54,11 @@ class SimulacaoResourceTest {
                 .when()
                 .post("/simulacoes")
                 .then()
-                .statusCode(400);
+                .statusCode(400)
+                .body("mensagem", equalTo("Falha de validacao"))
+                .body("detalhes.size()", greaterThan(0))
+                .body("detalhes", hasItem(containsString("valorInicial")))
+                .body("detalhes", hasItem(containsString("prazoMeses")));
     }
 
     @Test
@@ -60,7 +67,9 @@ class SimulacaoResourceTest {
                 .when()
                 .get("/simulacoes/99999")
                 .then()
-                .statusCode(404);
+                .statusCode(404)
+                .body("mensagem", containsString("nao encontrada"))
+                .body("detalhes", hasSize(0));
     }
 
     @Test
@@ -92,5 +101,112 @@ class SimulacaoResourceTest {
                 .statusCode(200)
                 .body("id", equalTo((int) id))
                 .body("memoriaCalculo", hasSize(2));
+    }
+
+    @Test
+    void deveRejeitarPrazoMesesForaDosLimites() {
+        String payloadPrazoZero = """
+                {
+                  "valorInicial": 1000.00,
+                  "taxaJurosMensal": 1.5,
+                  "prazoMeses": 0
+                }
+                """;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(payloadPrazoZero)
+                .when()
+                .post("/simulacoes")
+                .then()
+                .statusCode(400)
+                .body("mensagem", equalTo("Falha de validacao"))
+                .body("detalhes.size()", greaterThan(0))
+                .body("detalhes", hasItem(containsString("prazoMeses")));
+
+        String payloadPrazoAcima = """
+                {
+                  "valorInicial": 1000.00,
+                  "taxaJurosMensal": 1.5,
+                  "prazoMeses": 601
+                }
+                """;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(payloadPrazoAcima)
+                .when()
+                .post("/simulacoes")
+                .then()
+                .statusCode(400)
+                .body("mensagem", equalTo("Falha de validacao"))
+                .body("detalhes.size()", greaterThan(0))
+                .body("detalhes", hasItem(containsString("prazoMeses")));
+    }
+
+    @Test
+    void deveRejeitarValorInicialComMaisDeDuasCasasDecimais() {
+        String payload = """
+                {
+                  "valorInicial": 1000.001,
+                  "taxaJurosMensal": 1.5,
+                  "prazoMeses": 12
+                }
+                """;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/simulacoes")
+                .then()
+                .statusCode(400)
+                .body("mensagem", equalTo("Falha de validacao"))
+                .body("detalhes.size()", greaterThan(0))
+                .body("detalhes", hasItem(containsString("valorInicial")));
+    }
+
+    @Test
+    void deveRejeitarTaxaJurosComMaisDeQuatroCasasDecimais() {
+        String payload = """
+                {
+                  "valorInicial": 1000.00,
+                  "taxaJurosMensal": 1.23456,
+                  "prazoMeses": 12
+                }
+                """;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/simulacoes")
+                .then()
+                .statusCode(400)
+                .body("mensagem", equalTo("Falha de validacao"))
+                .body("detalhes.size()", greaterThan(0))
+                .body("detalhes", hasItem(containsString("taxaJurosMensal")));
+    }
+
+    @Test
+    void deveAceitarLimitesMinimosValidos() {
+        String payload = """
+                {
+                  "valorInicial": 0.01,
+                  "taxaJurosMensal": 0.0001,
+                  "prazoMeses": 1
+                }
+                """;
+
+        given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/simulacoes")
+                .then()
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("prazoMeses", equalTo(1))
+                .body("memoriaCalculo", hasSize(1));
     }
 }
